cb3b1e0c69698220b1fbc47c946bc140dc16dfd7
Gary <Gary.Dix@enform.nz>
2022-05-18 11:34:19 +1200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSD605MVCSecurityRoles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DSD605MVCSecurityRoles.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
